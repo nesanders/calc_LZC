10a8dd56192c2c66468d0b918aae7a778f5b4a8b
Nathan Sanders <nes@frankenmuth.(none)>
2012-10-19 11:53:22 -0400

    Using setuptools for install; tested as working

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='calc_LZC',
     version='0.1.0',
     author='N. E. Sanders',
     author_email='nsanders@cfa.harvard.edu',
-    packages=['calc_LZC'],
-    package_data={'calc_LZC':['calc_LZC/LZCpar.p']},
-    scripts=['calc_LZC/calc_LZC.py'],
+    packages=['LZC'],
+    package_dir={'LZC':'LZC'},
+    #package_data={'LZC':['LZC/LZCpar.p']},
+    data_files=[('LZC',['LZC/LZCpar.p'])],
+    include_package_data=True,
+    scripts=['LZC/LZC.py'],
     url='https://www.cfa.harvard.edu/~nsanders/papers/LZC/summary.htm',
     license='LICENSE.txt',
     description='Calculate photometric galaxy metallicities.',
